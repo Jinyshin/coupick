1d0f73fa71f2df44ac560eee6e2fae5f0f24a439
박상우 <parks@Parks-Mac.local>
2024-08-25 11:06:59 +0900

    tmp

--- server/src/polls/polls.schema.ts
+++ server/src/polls/polls.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema({ timestamps: true })
+@Schema({})
 export class Poll extends Document {
   @Prop({ required: true })
   price: number;
